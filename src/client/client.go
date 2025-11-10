0bc5448291605b14248ff754d52dd0cef967b4e0
camilo.gonzalez <andgonzalezcas@unal.edu.co>
2025-11-09 21:37:00 -0500

    feat(app): update client to have test on module

--- src/client/client.go
+++ src/client/client.go
@@ -30,37 +30,32 @@ func createConnection(host, port, connType string) net.Conn {
 	return conn
 }
 
-func main() {
-	// 1. se abre el archivo para guardar los resultados
-	logFile, err := os.Create(LOG_FILE)
+func createFileConnection(file string, init_time string) *os.File {
+	logFile, err := os.Create(file + init_time)
 	if err != nil {
 		fmt.Println("Error al crear el archivo de log:", err)
 		os.Exit(1)
 	}
-	defer logFile.Close()
 
-	fmt.Fprintf(logFile, "--- Inicio de prueba: %s ---\n", time.Now().Format(time.RFC3339))
-	logFile.WriteString("Iteración | Latencia (ms) | Resultado\n")
+	fmt.Fprintf(logFile, "--- Inicio de prueba: %s ---\n", init_time)
+	return logFile
+}
 
-	// 2. Se conecta al servidor
-	conn := createConnection(CONN_HOST, CONN_PORT, CONN_TYPE)
-	defer conn.Close()
-
+func runLatencyTest(conn net.Conn, numRequests int, logFile *os.File) time.Duration {
 	var totalLatency time.Duration
 	buffer := make([]byte, MAX_MESSAGE_SIZE)
 
-	// 3. Bucle de prueba de latencia
 	for i := 1; i <= NUM_REQUESTS; i++ {
 		messageBytes := fmt.Appendf(nil, "%s %d", MESSAGE, i)
 		start := time.Now()
 
-		_, err = conn.Write(messageBytes)
+		_, err := conn.Write(messageBytes)
 		if err != nil {
 			fmt.Printf("Error al enviar estímulo en iteración %d: %s.\n", i, err.Error())
 			break
 		}
 
-		_, err := conn.Read(buffer)
+		_, err = conn.Read(buffer)
 		if err != nil {
 			fmt.Printf("Error al leer request en iteración %d: %s.\n", i, err.Error())
 			break
@@ -79,7 +74,10 @@ func main() {
 		}
 	}
 
-	// 4. Cálculo de latencia promedio
+	return totalLatency
+}
+
+func createFinalReport(logFile *os.File, totalLatency time.Duration) {
 	avgLatency := totalLatency / time.Duration(NUM_REQUESTS)
 	avgLatencyMs := float64(avgLatency.Microseconds()) / 1000.0
 
@@ -90,4 +88,21 @@ func main() {
 
 	fmt.Fprintf(logFile, "--- Latencia Promedio: %.3f ms ---\n", avgLatencyMs)
 	fmt.Printf("Revisa el archivo '%s' para ver los resultados.\n", LOG_FILE)
+}
+
+func main() {
+	// 1. se abre el archivo para guardar los resultados
+	logFile := createFileConnection(LOG_FILE, time.Now().Format(time.RFC3339))
+	logFile.WriteString("Iteración | Latencia (ms) | Resultado\n")
+	defer logFile.Close()
+
+	// 2. Se conecta al servidor
+	conn := createConnection(CONN_HOST, CONN_PORT, CONN_TYPE)
+	defer conn.Close()
+
+	// 3. Bucle de prueba de latencia
+	totalLatency := runLatencyTest(conn, NUM_REQUESTS, logFile)
+
+	// 4. Cálculo de latencia promedio
+	createFinalReport(logFile, totalLatency)
 }
